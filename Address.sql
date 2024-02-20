9f55b98070a6e1a3a3b512d969ebbb1d7c96c2ed
ashley-colbert <136830986+ashley-colbert@users.noreply.github.com>
2024-02-20 11:06:50 -0330

    create statements

---
+++ Address.sql
@@ -0,0 +1,11 @@
+CREATE TABLE public."Address"
+(
+    address_id serial NOT NULL,
+    street_add character varying(32) NOT NULL,
+    prov character varying(12) NOT NULL,
+    postal_case character varying(7) NOT NULL,
+    PRIMARY KEY (address_id)
+);
+
+ALTER TABLE IF EXISTS public."Address"
+    OWNER to postgres;
---
+++ Receipt.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS public."Receipt"
+(
+    receipt_id integer NOT NULL DEFAULT nextval('"Receipt_receipt_id_seq"'::regclass),
+    payment_method character varying COLLATE pg_catalog."default" NOT NULL,
+    payment_date date NOT NULL,
+    amount_paid integer NOT NULL,
+    CONSTRAINT "Receipt_pkey" PRIMARY KEY (receipt_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."Receipt"
+    OWNER to postgres;
